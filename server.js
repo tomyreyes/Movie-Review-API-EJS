c11b5ec689d5fd669dd2002730db857cb9f6f77c
Tomy <tomyleereyes@gmail.com>
2018-02-09 16:14:33 -0800

    removed bug that crashed app

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ request(options, function (error, response, body) {
 
     if (error) throw new Error(error);
 
-    fs.writeFileSync('list.txt', JSON.stringify(body.results), (err) => {
+    fs.writeFile('list.txt', JSON.stringify(body.results), (err) => {
     })
 });
 
 
 
 let movieDb
-fs.readFileSync('list.txt', 'UTF8', (err, data)=>{
+fs.readFile('list.txt', 'UTF8', (err, data)=>{
     movieDb = JSON.parse(data)
 })
 
